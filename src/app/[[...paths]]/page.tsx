7f8ea340297591ddcf48f7783134338e00f46db8
Adam Butterworth <adam@equ.com.au>
2025-03-27 15:11:58 +0800

    Initial commit

---
+++ src/app/[[...paths]]/page.tsx
@@ -0,0 +1,27 @@
+import type { RouteParams, SearchParams } from "@nextwp/core";
+import {
+  WordpressTemplate,
+  generateStaticParams as nextWpStaticParams,
+} from "@nextwp/core";
+import templates from "@/templates";
+
+export default function PageRoute(props: {
+  params: RouteParams;
+  searchParams?: SearchParams;
+}) {
+  return (
+    <WordpressTemplate
+      params={props.params}
+      searchParams={props.searchParams}
+      templates={templates}
+    />
+  );
+}
+
+export { generateMetadata } from "@nextwp/core";
+
+export async function generateStaticParams() {
+  return nextWpStaticParams({
+    postTypes: ["pages", "posts"],
+  });
+}
